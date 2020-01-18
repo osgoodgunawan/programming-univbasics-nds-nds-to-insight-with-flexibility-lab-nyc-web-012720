69c2dab3cff2c0a359f64bb909e4384a9fcd21c3
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-18 01:24:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -103,6 +103,11 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
+  array=[]
+  counter=0
+  while counter<source.length
+  end
+
 end
 
 # ----------------    End of Your Code Region --------------------
